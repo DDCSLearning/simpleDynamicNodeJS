d30ad2f5344f28764ad4808c710957a4f763e9e1
Daniel Deutsch <deudan1010@gmail.com>
2017-05-02 22:30:47 +0200

    set home route

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  homeRoute(req,res);
 });
+
+function homeRoute (req, res) {
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Set Home Route\n');
+  }
+}
 
 server.listen(port, () => {
   console.log(`Server running at http://:${port}/`);
