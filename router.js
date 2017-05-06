4b59d8fa337bdc0a53f59f7da2a1f404c5c7a269
Daniel Deutsch <deudan1010@gmail.com>
2017-05-06 16:06:55 +0200

    add if else statment for POST body

--- router.js
+++ router.js
@@ -1,40 +1,48 @@
 const Profile = require("./profile.js");
 var render = require("./render");
-var commonHeader = {'Content-Type': 'html'};
+var querystring = require("querystring");
+var commonHeader = { 'Content-Type': 'html' };
 
-function home (req, res) {
+function home(req, res) {
   if (req.url === "/") {
-    res.writeHead(200, commonHeader);
-    render.view("header", {}, res);
-    render.view("search", {}, res);
-    render.view("footer", {}, res);
-    res.end('End of the response\n');
+    if (req.method.toLowerCase() === "get") {
+      res.writeHead(200, commonHeader);
+      render.view("header", {}, res);
+      render.view("search", {}, res);
+      render.view("footer", {}, res);
+      res.end('End of the response\n');
+
+    } else {
+      req.on("data", function (postBody) {
+        var query = querystring.parse(postBody.toString());
+        res.write(query.username);
+        res.end();
+      })
+    }
   }
 }
 
-function user (req, res) {
+function user(req, res) {
   var username = req.url.replace("/", "");
-  if (username.length > 0 ) {
-    
-
+  if (username.length > 0) {
     var studentProfile = new Profile(username)
-    studentProfile.on('end', function(data) {
+    studentProfile.on('end', function (data) {
       var values = {
-        avatarUrl: data.gravatar_url, 
+        avatarUrl: data.gravatar_url,
         username: data.profile_name,
         badges: data.badges.length,
         javascript: data.points.JavaScript
       }
-    res.writeHead(200, commonHeader);
-    render.view("header", {}, res);
-    render.view("profile", values, res);
-    render.view("footer", {}, res);
+      res.writeHead(200, commonHeader);
+      render.view("header", {}, res);
+      render.view("profile", values, res);
+      render.view("footer", {}, res);
       res.end('End of the response\n');
     });
 
-    studentProfile.on('error', function(err) {
+    studentProfile.on('error', function (err) {
       render.view("header", {}, res);
-      render.view("error", {errorMessage: err.message}, res)
+      render.view("error", { errorMessage: err.message }, res)
       render.view("search", {}, res);
       render.view("footer", {}, res);
       res.end('End of the response\n');
